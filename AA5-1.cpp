95673c5bfd635487b56a22df4e05aebfce465b59
elisor <easorato@gmail.com>
2014-02-28 07:22:19 -0300

    Update AA5-1.cpp

--- AA5-1.cpp
+++ AA5-1.cpp
@@ -1,1 +1,22 @@
-//Tentando entender como funciona!
+/*
+Nome:
+Elivelton Andreati Sorato - 545015
+Marcelo Miky Mine - 189669
+Willian Roberto de Moraes - 545228
+
+ Imagine uma fila de pessoas sentadas, onde não há a necessidade de ficar em pé um atrás do outro aguardando sua vez. Cada um tem uma senha (como nos bancos, atualmente) que indica sua posição na fila. A senha pode ser numérica ou alfa-numérica (você escolhe).
+ Os funcionários dos bancos atendem o cliente apertando um botãozinho que apresenta em um display o número da senha do próximo cliente. Os números não precisam estar ordenados sequencialmente. A senha 1 já pode ter sido utilizada por alguém e agora será reutilizada. Assim, o que importa é a posição na fila. É atendido sempre cliente que está na primeira posição. Todos os outros clientes sobem uma posição quando o primeiro deixa a fila (o segundo vira primeiro, o terceiro vira segundo e assim por diante).
+ Como cada banco tem um determinado número de cadeiras (tamanho da fila), você deverá implementar um sistema de fila (pense nos métodos da classe fila) que se adeque ao banco (tenha o tamanho que o banco deseje).
+ Para isso, implemente a fila com alocação dinâmica de memória.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    
+    return 0;
+}
